435f9e35cbb1bfd5f5c3b86086157a6cb6dc1347
Juan <juan21ramos@gmail.com>
2025-03-05 17:48:21 -0400

    sub categoria en resumen

--- src/views/reporte/resumen/components/resumen_categoria/index.vue
+++ src/views/reporte/resumen/components/resumen_categoria/index.vue
@@ -14,7 +14,7 @@
           <ion-item>
             <ion-label horizontal="center"><ion-icon :icon="add"></ion-icon><strong>{{ fila.nombre_categoria }}</strong></ion-label>
             <ion-label horizontal="center"><strong>{{ formatNumber(fila.monto_divisas, 2) }}</strong> </ion-label>
-            <ion-label slot="end"> <ion-fab-button color="light"  size="small" :disabled="progress"><ion-icon :icon="eye" size="small" color="success"  @click="mostrarResumen(fila.id)"></ion-icon></ion-fab-button></ion-label>
+            <ion-label slot="end"> <ion-fab-button color="light"  size="small" :disabled="progress"><ion-icon :icon="eye" size="small" color="success"  @click="mostrarResumenSub(fila)"></ion-icon></ion-fab-button></ion-label>
           </ion-item>
         </template>
       </ion-list>
@@ -26,7 +26,7 @@
       </ion-infinite-scroll> -->
       
   
-      <!-- <resumenSubcategoria v-if="isOpen"/> -->
+      <resumenSubcategoria v-if="isOpenSub" :data="dataSub"  @cerrarDetalleSubCategoria="cerrarSubDetalle"/>
       
     </ion-content>
    
@@ -38,6 +38,7 @@
 
 import { IonContent, IonItem, IonHeader, IonToolbar, IonButtons, IonTitle, IonModal, IonButton, IonInput, IonToast, IonSelect, IonSelectOption } from '@ionic/vue';
 import { defineEmits, ref , onMounted, watch } from 'vue';
+  import resumenSubcategoria from './resumen_subcategoria/index.vue'
 import  {formatNumber} from '../../../../../utils/calc.js';
 import { eye } from 'ionicons/icons';
 import lodash from 'lodash';
@@ -46,12 +47,29 @@ const props = defineProps(['data','id']);
 const emit = defineEmits(['cerrarDetalleCategoria']);
 const dataCategorias=ref([]);
 const titulo=ref('');
+const isOpenSub=ref(false);
+const dataSub=ref([]);
 
 onMounted(async ()=>{
 
     dataCategorias.value=lodash.orderBy(props.data.categorias, ["monto_divisas"], ["desc"]);
+   // console.log("prueba",dataCategorias.value),
     titulo.value=props.data.tipo_movimiento;
 });
+
+
+const mostrarResumenSub = async (fila) => {
+    //isOpen.value=true;
+    //console.log("fila",fila.subcategorias);
+    isOpenSub.value=true;
+    dataSub.value=fila.subcategorias;
+    // objCategorias.value.tipo_movimiento=fila.tipo_movimiento;
+    // objCategorias.value.categorias=fila.categorias;
+};
+
+const cerrarSubDetalle = (()=>{
+  isOpenSub.value=false;
+  });
 
 
 
